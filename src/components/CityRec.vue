1621e4c11d0584d8968111e3950bf10ca07ee24e
hiiamanop <naufalmuzakki895@gmail.com>
2022-08-13 17:17:08 +0700

    main

---
+++ src/components/CityRec.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="CityRec">
+    <div class="container">
+      <!-- bagian judul -->
+      <div class="row justify-content-between">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4 text-center rekomendasi">
+          <h5>Rekomendasi Hotel Berdasarkan Kota</h5>
+        </div>
+        <div class="col-lg-4 text-end">
+          <router-link to="ListView" href="">Lihat Semua</router-link>
+        </div>
+      </div>
+      <!-- akhir bagian judul -->
+
+      <!-- Kota -->
+      <div class="row kota mt-5">
+        <div class="col-lg-3 mt-2">
+          <div class="bg-KotaSatu shadow">
+            <div class="row">
+              <div class="col Kota">
+                <h3>Kota Palembang</h3>
+                <h5>110+ akomodasi</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 mt-2">
+          <div class="bg-KotaDua shadow">
+            <div class="row">
+              <div class="col Kota">
+                <h3>Kota Yogya</h3>
+                <h5>110+ akomodasi</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 mt-2">
+          <div class="bg-KotaTiga shadow">
+            <div class="row">
+              <div class="col Kota">
+                <h3>Kota Denpasar</h3>
+                <h5>110+ akomodasi</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 mt-2">
+          <div class="bg-KotaEmpat shadow">
+            <div class="row">
+              <div class="col Kota">
+                <h3>Kota jakarta</h3>
+                <h5>110+ akomodasi</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Akhir bagian Kota -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityRec",
+};
+</script>
+
+<style>
+#CityRec .bg-KotaSatu {
+  background-image: url("/src/assets/palembang.png");
+  width: 100%;
+  height: auto;
+  background-size: cover;
+  border-radius: 15px;
+  margin-bottom: 20px;
+}
+#CityRec .bg-KotaDua {
+  background-image: url("/src/assets/yogya.png");
+  width: 100%;
+  height: auto;
+  background-size: cover;
+  border-radius: 15px;
+  margin-bottom: 20px;
+}
+#CityRec .bg-KotaTiga {
+  background-image: url("/src/assets/denpasar.png");
+  width: 100%;
+  height: auto;
+  background-size: cover;
+  border-radius: 15px;
+  margin-bottom: 20px;
+}
+#CityRec .bg-KotaEmpat {
+  background-image: url("/src/assets/jakarta.png");
+  width: 100%;
+  height: auto;
+  background-size: cover;
+  border-radius: 15px;
+  margin-bottom: 20px;
+}
+
+</style>
---
+++ src/components/HotelRec.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="HotelRec">
+    <div class="container">
+      <!-- bagian judul -->
+      <div class="row justify-content-between mt-5">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4 text-center rekomendasi mb-3">
+          <h5>Rekomendasi Hotel</h5>
+        </div>
+        <div class="col-lg-4 text-end">
+          <router-link to="ListView" href="">Lihat Semua</router-link>
+        </div>
+      </div>
+      <!-- akhir bagian judul -->
+
+      <!-- card -->
+      <div class="row justify-content-between mt-4">
+        <div class="col-lg-4">
+          <div class="card shadow" style="width: 100%">
+            <img src="../assets/amaris.png" class="img" alt="..." />
+            <div class="card-body">
+              <h5 class="Hotel Amaris">Amaris Hotel Palembang</h5>
+              <p class="Harga Amaris">Rp.300k,-/malam</p>
+              <div class="row">
+                <div class="col text-start">
+                  <router-link to="PemesananView" href="#" class="btn btn-primary">Pesan Sekarang</router-link>
+                </div>
+                <div class="col text-end mt-2">
+                  <img src="../assets/Star1.png" alt="" />
+                  <span>4.6 Rb</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card shadow" style="width: 100%">
+            <img src="../assets/arista.png" class="img" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Arista Hotel Palembang</h5>
+              <p class="card-text">Rp.300k,-/malam</p>
+              <div class="row">
+                <div class="col text-start">
+                  <router-link to="PemesananView" class="btn btn-primary">Pesan Sekarang</router-link>
+                </div>
+                <div class="col text-end mt-2">
+                  <img src="../assets/Star1.png" alt="" />
+                  <span>4.6 Rb</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card shadow" style="width: 100%">
+            <img src="../assets/alts.png" class="img" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">The Alts Hotel Palembang</h5>
+              <p class="card-text">Rp.300k,-/malam</p>
+              <div class="row">
+                <div class="col text-start">
+                  <router-link to="PemesananView" href="#" class="btn btn-primary">Pesan Sekarang</router-link>
+                </div>
+                <div class="col text-end mt-2">
+                  <img src="../assets/Star1.png" alt="" />
+                  <span>4.6 Rb</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- akhir bagian card -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HotelRec",
+};
+</script>
+
+<style>
+</style>
